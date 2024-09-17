0dee651f26b9f252cb21e7b29ac137629683ce3a
CashMoneyMichael M <mmikey1010@gmail.com>
2024-09-17 18:20:14 +0000

    added random generator

--- Worlde.py
+++ Worlde.py
@@ -1,4 +1,10 @@
-word = "great"
+import random
+
+possible_words = ["great", "swift", "slime", "break", "quake"]
+
+
+word = random.choice(possible_words)
+
 
 default = '\033[0m'
 green = '\033[92m'
